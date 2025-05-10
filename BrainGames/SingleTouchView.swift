97d0d32dc2dc0ec97774b9547e12fc92b1444616
Dansterhamster <dbyrd38@gatech.edu>
2025-05-10 05:43:45 -0400

    Added Home Screen

---
+++ BrainGames/SingleTouchView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleTouchView.swift
+//  BrainGames
+//
+//  Created by Danny Byrd on 5/10/25.
+//
+
+import SwiftUI
+
+struct SingleTouchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleTouchView()
+}
